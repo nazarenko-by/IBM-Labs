3457fdf527be0e267b1544600017a89df766a6cf
nazarenko-by <bodean56@ukr.net>
2025-06-11 23:12:32 +0300

    lab 20: add QuizApplication

---
+++ QuizApplication/src/QuizDetails.js
@@ -0,0 +1,43 @@
+import React, { useMemo } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+
+const QuizDetails = () => {
+    const { questions, currentQuestionIndex } = useSelector((state) => state.quiz);
+    const currentCount = useMemo(() => (currentQuestionIndex < questions.length ? currentQuestionIndex + 1 : currentQuestionIndex), [ currentQuestionIndex ])    
+    return (
+        <View style={styles.detailsContainer}>
+            <Text style={styles.detailsText} testID='currentQuestion'>
+                Question {currentCount} of {questions.length}
+            </Text>
+            <Text style={styles.detailsText} testID='totalCurrentQuestion'>
+                Total Questions: {questions.length}
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    marginBottom: 20,
+    alignItems: 'center',
+    padding: 15,
+    backgroundColor: '#E0E0E0',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 2.5,
+    elevation: 5,
+  },
+  detailsText: {
+    fontSize: 18,
+    color: '#333',
+    fontWeight: '500',
+  },
+});
+
+export default QuizDetails;
---
+++ QuizApplication/src/store.js
@@ -0,0 +1,36 @@
+// src/store.js
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import quizQuestions from './quizQuestions'; // Fix the import
+
+// Create a quiz slice
+const quizSlice = createSlice({
+  name: 'quiz',
+  initialState: {
+    questions: quizQuestions,
+    currentQuestionIndex: 0,
+    score: 0,
+  },
+  reducers: {
+    answerQuestion: (state, action) => {
+        if (state.questions[state.currentQuestionIndex].answer === action.payload) {
+            state.score += 1;
+        }
+        state.currentQuestionIndex += 1;
+    },
+    resetQuiz: (state, action) => {
+        state.score = 0;
+        state.currentQuestionIndex = 0;
+    }
+  },
+});
+
+// Export actions
+export const { answerQuestion, resetQuiz } = quizSlice.actions;
+// Create and export the store
+const store = configureStore({
+  reducer: {
+    quiz: quizSlice.reducer,
+  },
+});
+
+export default store;
